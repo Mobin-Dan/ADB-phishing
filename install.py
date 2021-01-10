a28aa788e5f3c4d3aae1cbc8a191cfdca77f6375
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-11 00:03:18 +0330

    Create install.py

---
+++ install.py
@@ -0,0 +1,2 @@
+import os
+os.system("apt update ; pkg install python ; pip install colorama ; pip install ; git clone https://github.com/Mobin-Dan/ADB-phishing ; cd ADB-phishing ; python ADB-phishing.py")
