db6763022c6c2de1c1e013b4ceac324168451d88
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-10 16:26:52 +0330

    Create ADB-phishing.py

---
+++ ADB-phishing.py
@@ -0,0 +1,15 @@
+import os
+import pyfiglet
+from colorama import Fore
+os.system("clear")
+out = pyfiglet.figlet_format("ADB PHIS", font="3-d")
+print(Fore.WHITE+"*"*70)
+print(Fore.RED+  out)
+print(Fore.WHITE+"*"+Fore.GREEN+"[+]"+Fore.RED+"INSTAGRAM")
+print(Fore.WHITE+"*"+Fore.GREEN+"[+]"+Fore.RED+"telenumber")
+print(Fore.WHITE+"*"*70)
+s =input(Fore.RED+"="+Fore.GREEN+"telenumber/instagram"+Fore.RED+">")
+if s == "instagram":
+        os.system("python module2.py")
+if s == "telenumber":
+        os.system("python module1.py")
