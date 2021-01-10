bbffe105ff44dab8ae8be4d01164e9e0b7c8ee03
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-10 16:28:14 +0330

    Create module2.py

---
+++ module2.py
@@ -0,0 +1,11 @@
+import os
+import pyfiglet
+from colorama import Fore
+print(Fore.GREEN)
+os.system("clear")
+ins =pyfiglet.figlet_format("INstaHack", font="3-d")
+print(Fore.RED+ins)
+print
+print(Fore.GREEN)
+print("Goodbye until the next update:)")
+os.system("php -S localhost:8090 -t insta_hack")
