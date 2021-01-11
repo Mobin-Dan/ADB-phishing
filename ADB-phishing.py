219bf596d488f1e5474787110574197d4f765958
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-11 12:59:23 +0330

    Update ADB-phishing.py

--- ADB-phishing.py
+++ ADB-phishing.py
@@ -7,7 +7,18 @@ os.system("unzip BTC.zip")
 os.system("clear")
 out = pyfiglet.figlet_format("ADB PHIS", font="3-d")
 print(Fore.WHITE+"*"*70)
-print(Fore.RED+  out)
+print(Fore.RED+"""
+
+  ___ ____________              _     _     _     _             
+ / _ \|  _  \ ___ \            | |   (_)   | |   (_)            
+/ /_\ \ | | | |_/ /______ _ __ | |__  _ ___| |__  _ _ __   __ _ 
+|  _  | | | | ___ \______| '_ \| '_ \| / __| '_ \| | '_ \ / _` |
+| | | | |/ /| |_/ /      | |_) | | | | \__ \ | | | | | | | (_| |
+\_| |_/___/ \____/       | .__/|_| |_|_|___/_| |_|_|_| |_|\__, |
+                         | |                               __/ |
+                         |_|                              |___/ 
+                                                                
+""")
 print(Fore.WHITE+"*"+Fore.GREEN+"[+]"+Fore.RED+"INSTAGRAM")
 print(Fore.WHITE+"*"+Fore.GREEN+"[+]"+Fore.RED+"telenumber")
 print(Fore.WHITE+"*"+Fore.GREEN+"[+]"+Fore.RED+"BTC")
