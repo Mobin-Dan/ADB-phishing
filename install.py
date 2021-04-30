78c4994ce1405010d65b258de3ec3fc15bf95aa9
Mobin-Dan <68151748+Mobin-Dan@users.noreply.github.com>
2021-04-30 11:54:13 +0430

    Delete install.py

--- install.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("apt update ; pkg install python ; pip install colorama ; pip install ; git clone https://github.com/Mobin-Dan/ADB-phishing ; cd ADB-phishing ; python ADB-phishing.py")
